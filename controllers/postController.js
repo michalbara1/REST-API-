f90891516d2f57479b0335ba557c693b696059ff
Michal <michalbara1@gmail.com>
2024-11-17 19:12:36 +0200

    added get post by sender ID

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,11 +2,15 @@ const Post = require('../models/postModel');
 
 exports.createPost = async (req, res) => {
   try {
-    const { title, content, owner } = req.body; 
+    const { title, content, owner, senderId } = req.body;
+    if (!senderId) {
+      return res.status(400).json({ message: "senderId is required" });
+    }
     const newPost = new Post({
       title,
       content,
       owner,
+      senderId,
     });
     await newPost.save();
     res.status(201).json({
@@ -17,6 +21,34 @@ exports.createPost = async (req, res) => {
     console.error(error);
     res.status(500).json({
       message: 'Failed to create post',
+      error: error.message,
+    });
+  }
+};
+
+exports.getPostsBySender = async (req, res) => {
+  try {
+    const senderId = req.query.sender;  
+    let posts;
+    if (senderId) {
+      console.log('Sender ID:', senderId);
+      posts = await Post.find({ senderId: senderId });
+    } else {
+      posts = await Post.find();
+    }
+    if (posts.length === 0) {
+      return res.status(404).json({
+        message: 'No posts found for the specified sender',
+      });
+    }
+    res.status(200).json({
+      message: 'Posts fetched successfully',
+      posts: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'Failed to fetch posts',
       error: error.message,
     });
   }
@@ -39,7 +71,6 @@ exports.getPostById = async (req, res) => {
   try {
     const post = await Post.findById(postId); 
     if (!post) {
-      // If the post isn't found, return a 404 error
       return res.status(404).json({ message: 'Post not found' });
     }
     res.status(200).json({
@@ -54,3 +85,5 @@ exports.getPostById = async (req, res) => {
     });
   }
 };
+
+
